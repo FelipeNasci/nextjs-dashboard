326d3eb56ae69b09e1015ed35ad4c17c03bcdc7c
Diego Nascimento <dfnascimento@live.com>
2024-06-19 20:01:03 -0300

    feat(resources): add get-image function

---
+++ src/resources/images/index.ts
@@ -0,0 +1,30 @@
+import { ImageResponse } from "@/domain/models/Images";
+import Image from "@/services/image";
+
+const DEFAULT_RESOURCE = "curated";
+
+const fallback = {
+  total_results: 0,
+  page: 0,
+  per_page: 0,
+  photos: [],
+  next_page: "",
+  prev_page: "",
+};
+
+async function getImages(filter?: string): Promise<ImageResponse> {
+  const resource = `/${filter || DEFAULT_RESOURCE}?per_page=15`;
+
+  try {
+    const response = await Image.fetch<ImageResponse>(resource);
+
+    if (!response.total_results) return fallback;
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return fallback;
+  }
+}
+
+export { getImages };

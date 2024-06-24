3761d170b4d9ed5505de7b1eefa3690ddd4019fa
Diego Nascimento <dfnascimento@live.com>
2024-06-24 07:36:43 -0300

    feat: add pages and search

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -34,7 +34,7 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              { 'bg-blue-400 text-white': pathname === link.href },
+              { 'bg-slate-950 text-yellow-400': pathname === link.href },
             )}
           >
             <LinkIcon className="w-6" />
